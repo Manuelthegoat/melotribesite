b2402e4104adbf1bb3d6657efa6519a4d31fdb23
manuelthegoat <manuelcreation247@gmail.com>
2024-02-13 11:53:36 -0800

    gg

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="footer">
+        <div className="footeritem">
+          <div className="foot">
+            <p className="foottitle">Get Started</p>
+            <p className="footertext">Get This Theme</p>
+            <p className="footertext">Read Documents</p>
+            <p className="footertext">Get Supported</p>
+            <p className="footertext">More Music Themes</p>
+          </div>
+          <div className="foot">
+            <p className="foottitle">Browse & Explore</p>
+            <p className="footertext">Streaming Library</p>
+            <p className="footertext">Live Radio Stations</p>
+            <p className="footertext">Icecast</p>
+            <p className="footertext">Shoutcast</p>
+            <p className="footertext">Youtube Music</p>
+            <p className="footertext">Press</p>
+          </div>
+          <div className="foot">
+            <p className="foottitle">Account</p>
+            <p className="footertext">Sign Up</p>
+            <p className="footertext">Verified on Melotribe</p>
+            <p className="footertext">Start Upload</p>
+          </div>
+          <div className="foot">
+            <p className="foottitle">About</p>
+            <p className="footertext">About Us</p>
+            <p className="footertext">Contact</p>
+            <p className="footertext">Terms</p>
+            <p className="footertext">Service Policy</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
